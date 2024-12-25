c8fc09c26f9b1f7b588c79f85a32447fad8edf42
Zagrthos <36556009+Zagrthos@users.noreply.github.com>
2024-12-25 23:14:12 +0100

    Update packages, add default sizes and remove not needed code

--- src/ChatManager.Next/MainWindow.xaml.cs
+++ src/ChatManager.Next/MainWindow.xaml.cs
@@ -1,22 +1,15 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using Microsoft.Extensions.Logging;
 
 namespace ChatManager;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-[SuppressMessage("Minor Code Smell", "S2325:Methods and properties that don't access instance data should be static", Justification = "These are event methods.")]
 public sealed partial class MainWindow : Window
 {
-    private readonly ILogger<MainWindow> _logger;
-
-    public MainWindow(ILogger<MainWindow> logger)
+    public MainWindow()
     {
-        _logger = logger;
-
         InitializeComponent();
         GlobalTabItem.Focus();
     }
